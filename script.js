66f4ec658de680f401f91722c3eb8033d46b3314
Sehai <sahisahi4674@gmail.com>
2025-02-15 07:45:35 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-document.querySelector("form").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    let name = document.getElementById("name").value;
-    let phone = document.getElementById("phone").value;
-    let flavor = document.getElementById("flavor").value;
-
-    let whatsappURL = https://wa.me/085697445308?text=Halo,%20saya%20${name}%20ingin%20memesan%20Basreng%20rasa%20${flavor}.%20Nomor%20HP:%20${phone};
-
-    window.open(whatsappURL, "_blank");
-});
+<script>
+    function addToCart(product) {
+        alert(product + " telah ditambahkan ke keranjang!");
+    }
+</script>
